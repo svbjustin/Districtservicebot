be23520d55326859ba9cbc8f75c14496f8234ef1
svbjustin <xm870572@gmail.com>
2026-03-16 14:55:36 +0100

    Add config loader

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const configPath = path.join(__dirname, "config.json");
+const raw = fs.readFileSync(configPath, "utf-8");
+const config = JSON.parse(raw);
+
+export default config;
